2af3029b6d1956f1bb07b8f96d0647fbdecdc750
Oğuz Çetin <oguzcetin674@gmail.com>
2026-06-22 21:35:10 +0300

    Add YummyEvent entity, API, and dynamic UI components
    Introduced a new `YummyEvent` entity with its database schema, API endpoints, and dynamic rendering in the UI. Replaced hardcoded sections for "Events," "Chefs," and the footer in `Index.cshtml` with reusable view components. Added DTOs for structuring data and updated `_ViewImports.cshtml` to include them. Created a migration to add the `YummyEvents` table. Improved modularity and maintainability of the application.

---
+++ APIProject6.WebAPI/Controllers/YummyEventsController.cs
@@ -0,0 +1,60 @@
+﻿using APIProject6.WebAPI.Context;
+using APIProject6.WebAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject6.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YummyEventsController : ControllerBase
+    {
+        private readonly APIContext _context;
+        public YummyEventsController(APIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult YummyEventList()
+        {
+            var values = _context.YummyEvents.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateYummyEvent(YummyEvent YummyEvent)
+        {
+            _context.YummyEvents.Add(YummyEvent);
+            _context.SaveChanges();
+            return Ok("The YummyEvent has been added.");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            _context.YummyEvents.Remove(value);
+            _context.SaveChanges();
+            return Ok("YummyEvent has been deleted.");
+        }
+
+        [HttpGet("GetYummyEvent")]
+        public IActionResult GetYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+
+            return Ok(value);
+        }
+
+        [HttpPut]
+
+        public IActionResult UpdateYummyEvent(YummyEvent YummyEvent)
+        {
+            _context.YummyEvents.Update(YummyEvent);
+            _context.SaveChanges();
+            return Ok("The YummyEvent has been updated.");
+        }
+    }
+}
---
+++ APIProject6.WebUI/ViewComponents/_ChefDeafaultComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using APIProject6.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace APIProject6.WebUI.ViewComponents
+{
+    public class _ChefDeafaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDeafaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7277/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
