39c92710c1f9bdbaa1fb3b3abfe580d2b1481d7e
Oğuz Çetin <oguzcetin674@gmail.com>
2026-07-07 00:13:33 +0300

    Refactor stats section and add dynamic data fetching
    Replaced hardcoded stats section with a dynamic implementation
    using `_HomePageStatisticComponentPartial`. Introduced a new
    `StatisticsController` API to provide product, reservation, chef,
    and guest counts. Updated `Default.cshtml` to include a helper
    function `ResolveImageUrl` for dynamic image URL resolution.
    
    Improved maintainability by modularizing the stats section and
    fetching data dynamically via HTTP requests. Enhanced the
    application's flexibility and scalability.

---
+++ APIProject6.WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,41 @@
+﻿using APIProject6.WebAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject6.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly APIContext _context;
+        public StatisticsController(APIContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            var value =_context.Products.Count();
+            return Ok(value);
+        }
+        [HttpGet("ReservationCount")]
+        public IActionResult ReservationCount()
+        {
+            var value = _context.Reservations.Count();  
+            return Ok(value);
+        }
+        [HttpGet("ChefCount")]
+        public IActionResult ChefCount()
+        {
+            var value = _context.Chefs.Count();
+            return Ok(value);
+        }
+        [HttpGet("TotalGuestCount")]
+        public IActionResult TotalGuestCount()
+        {
+            var value = _context.Reservations.Sum(x => x.CountOfPeople);
+            return Ok(value);
+        }
+    }
+}
